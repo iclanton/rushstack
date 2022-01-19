d1ce43535ede53c941433e9a13979ee94be724a0
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-01-18 17:28:17 -0800

    Remove "colors" package from Heft.

--- apps/heft/src/cli/test/CommandLineHelp.test.ts
+++ apps/heft/src/cli/test/CommandLineHelp.test.ts
@@ -2,21 +2,13 @@
 // See LICENSE in the project root for license information.
 
 import { AnsiEscape } from '@rushstack/node-core-library';
-import * as colorsPackage from 'colors';
 
 import { HeftCommandLineParser } from '../HeftCommandLineParser';
 
 describe('CommandLineHelp', () => {
-  let colorsEnabled: boolean;
-
   let parser: HeftCommandLineParser;
 
   beforeEach(() => {
-    colorsEnabled = colorsPackage.enabled;
-    if (!colorsEnabled) {
-      colorsPackage.enable();
-    }
-
     // ts-command-line calls process.exit() which interferes with Jest
     jest.spyOn(process, 'exit').mockImplementation((code?: number) => {
       throw new Error(`Test code called process.exit(${code})`);
@@ -26,12 +18,6 @@ describe('CommandLineHelp', () => {
     // if it encounters errors.
     // TODO Remove the calls to process.exit() or override them for testing.
     parser = new HeftCommandLineParser();
-  });
-
-  afterEach(() => {
-    if (!colorsEnabled) {
-      colorsPackage.disable();
-    }
   });
 
   it('prints the global help', () => {
