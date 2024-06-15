a287981365b67bc7e30a06d3c8085d8dc872c5b1
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2024-06-15 14:58:52 -0400

    Basic implementation of the github actions cache plugin.

--- rush-plugins/rush-github-actions-cache-plugin/src/GitHubActionsBuildCacheProvider.ts
+++ rush-plugins/rush-github-actions-cache-plugin/src/GitHubActionsBuildCacheProvider.ts
@@ -2,24 +2,55 @@
 // See LICENSE in the project root for license information.
 
 import type { ITerminal } from '@rushstack/terminal';
-import {
-  type ICloudBuildCacheProvider,
-  type RushSession,
-  EnvironmentConfiguration
-} from '@rushstack/rush-sdk';
+import { FileSystem } from '@rushstack/node-core-library';
+import type { ICloudBuildCacheProvider, RushConfiguration } from '@rushstack/rush-sdk';
+
+import * as cacheHttpClient from './ref/cacheHttpClient';
+import type { ArtifactCacheEntry, ITypedResponseWithError, ReserveCacheResponse } from './ref/contracts';
+
+const IS_SUPPORTED: boolean = !!process.env.ACTIONS_CACHE_URL;
 
 export class GitHubActionsBuildCacheProvider implements ICloudBuildCacheProvider {
+  private readonly _tempPath: string;
+
   public get isCacheWriteAllowed(): boolean {
-    return EnvironmentConfiguration.buildCacheWriteAllowed ?? false;
+    return IS_SUPPORTED;
   }
 
-  public constructor(rushSession: RushSession) {}
+  private constructor(tempPath: string) {
+    this._tempPath = tempPath;
+  }
+
+  public static async initializeAsync(
+    rushConfiguration: RushConfiguration
+  ): Promise<GitHubActionsBuildCacheProvider> {
+    const tempPath: string = `${rushConfiguration.commonTempFolder}/gh-actions-cache`;
+    await FileSystem.ensureEmptyFolderAsync(tempPath);
+    return new GitHubActionsBuildCacheProvider(tempPath);
+  }
 
   public async tryGetCacheEntryBufferByIdAsync(
     terminal: ITerminal,
     cacheId: string
   ): Promise<Buffer | undefined> {
-    throw new Error('Method not implemented.');
+    if (!IS_SUPPORTED) {
+      return undefined;
+    }
+
+    const augmentedCacheId: string = `000-${cacheId}`;
+    const tempPath: string = `${this._tempPath}/${augmentedCacheId}`;
+    const getCacheEntryResult: ArtifactCacheEntry | null = await cacheHttpClient.getCacheEntry(
+      [augmentedCacheId],
+      [tempPath]
+    );
+    terminal.writeLine(`Restore cache data for ${cacheId}: ${JSON.stringify(getCacheEntryResult)}`);
+    const archiveLocation: string | undefined = getCacheEntryResult?.archiveLocation;
+    if (archiveLocation) {
+      await cacheHttpClient.downloadCache(archiveLocation, tempPath);
+      const result: Buffer = await FileSystem.readFileToBufferAsync(tempPath);
+      await FileSystem.deleteFileAsync(tempPath);
+      return result;
+    }
   }
 
   public async trySetCacheEntryBufferAsync(
@@ -27,7 +58,39 @@ export class GitHubActionsBuildCacheProvider implements ICloudBuildCacheProvider
     cacheId: string,
     entryBuffer: Buffer
   ): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    const augmentedCacheId: string = `000-${cacheId}`;
+    const tempPath: string = `${this._tempPath}/${augmentedCacheId}`;
+    try {
+      await FileSystem.writeFileAsync(tempPath, entryBuffer, { ensureFolderExists: true });
+
+      const archiveFileSize: number = entryBuffer.length;
+      const reserveCacheResponse: ITypedResponseWithError<ReserveCacheResponse> =
+        await cacheHttpClient.reserveCache(augmentedCacheId, [tempPath], { cacheSize: archiveFileSize });
+
+      terminal.writeLine(`Reserve cache data for ${cacheId}: ${JSON.stringify(reserveCacheResponse)}`);
+
+      const newCacheId: number | undefined = reserveCacheResponse?.result?.cacheId;
+      if (newCacheId) {
+        await cacheHttpClient.saveCache(newCacheId, tempPath);
+        return true;
+      } else if (reserveCacheResponse?.statusCode === 400) {
+        throw new Error(
+          reserveCacheResponse?.error?.message ??
+            `Cache size of ~${Math.round(
+              archiveFileSize / (1024 * 1024)
+            )} MB (${archiveFileSize} B) is over the data cap limit, not saving cache.`
+        );
+      } else {
+        throw new Error(
+          `Unable to reserve cache with key ${augmentedCacheId}, another job may be creating this cache. More details: ${reserveCacheResponse?.error?.message}`
+        );
+      }
+    } catch (error) {
+      terminal.writeErrorLine(`Failed to save cache: ${error}`);
+      return false;
+    } finally {
+      await FileSystem.deleteFileAsync(tempPath);
+    }
   }
 
   public async updateCachedCredentialAsync(terminal: ITerminal, credential: string): Promise<void> {
